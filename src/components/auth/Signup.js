cb413ffc2382788bea2e7c842e33d04f7e27b0d6
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-01 07:58:46 -0700

    combine sign up

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -4,19 +4,22 @@ import LandlordSignup from "./LandlordSignup";
 import TenantSignup from "./TenantSignup";
 
 
-function Signup(){
+function Signup({signup}){
     
     return(
         <>
 
         <div className="login-wrap">
 	        <div className="login-html">
-		        <input id="tab-1" type="radio" name="tab" className="sign-in" checked/><label for="tab-1" className="tab">Tenant?</label>
+                <centre>
+                    <p id="register">Register as?</p>
+                </centre>
+		        <input id="tab-1" type="radio" name="tab" className="sign-in" checked/><label for="tab-1" className="tab">Tenant</label>
 		        <input id="tab-2" type="radio" name="tab" className="sign-up"/><label for="tab-2" className="tab">LandLord</label>
 		        <div className="login-form">
 
-                    <TenantSignup/>
-                    <LandlordSignup/>
+                    <TenantSignup signup={signup} />
+                    <LandlordSignup signup={signup} />
 
 		        </div>
 	        </div>
