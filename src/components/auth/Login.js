9776dd356f8bca223bb8ded4c4a2bcc1d1d7a2be
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-01 07:58:33 -0700

    conbin logins

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -4,20 +4,23 @@ import TenantLogin from "./TenantLogin";
 import LandlordLogin from "./LandlordLogin";
 
 
-function Login(){
+function Login({signup}){
 
     return(
         <>
 
         <div className="login-wrap">
 	        <div className="login-html">
-		        <input id="tab-1" type="radio" name="tab" className="sign-in" checked/><label for="tab-1" className="tab">Tenant?</label>
+                <centre>
+                    <p id="register">Login as?</p>
+                </centre>
+		        <input id="tab-1" type="radio" name="tab" className="sign-in" checked/><label for="tab-1" className="tab">Tenant</label>
 		        <input id="tab-2" type="radio" name="tab" className="sign-up"/><label for="tab-2" className="tab">LandLord</label>
 		        <div className="login-form">
 
 
-                    <TenantLogin/>
-                    <LandlordLogin/>
+                    <TenantLogin signup={signup}/>
+                    <LandlordLogin signup={signup}/>
 
 		        </div>
 	        </div>
