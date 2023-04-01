b837eb9f71e818effbe8c06316dcc2cbfc2ef623
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-01 07:52:11 -0700

    add routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Route, Routes, Link, Navigate} from "react-router-dom"
+import Home from "./components/pages/Home";
+import Landingpage from './components/pages/Landingpage';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
 
 function App() {
+
+
+  const [user, setUser] = useState(null);
+  const [anyUser, setAnyUser] = useState(false)
+
+  function signup(user) {
+		setUser(user);
+	}
+
+  function handleLogout() {
+    fetch("http://127.0.0.1:3000/logout", { method: "DELETE" }).then((r) => {
+      if (r.status.no_content) {
+        setUser(null);
+        setAnyUser(true)
+      }
+    });
+  }
+
+if(anyUser){
+  return <Navigate to="/"/>
+}
+  
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3 >
+        <Link to="/" id="logo_title">RealState</Link>  
+      </h3>
+
+
+
+
+      <Routes>
+        <Route path="/" element={<Landingpage/>}/>
+        <Route path="/home" element={<Home handleLogout={handleLogout} user={user}/>}/>
+
+        <Route path="/login" element={<Login signup={signup}/>} />
+
+        <Route path="/signup" element={<Signup signup={signup}/>} />
+
+
+
+
+        {/* <Route path="/login" element={
+       
+        !user ? <Login signup={signup} /> : <Navigate to="/home"></Navigate>
+       }/>
+
+      <Route path="/signup" element={
+       
+       !user ? <Signup signup={signup} /> : <Navigate to="/home"></Navigate>
+      }/> */}
+
+      </Routes>
+
+
+
+
+
+   
     </div>
   );
 }
