651f6c916304f33427d0679c36f2fc771ed2a302
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-02 00:27:57 -0700

    add houses page

--- src/components/pages/HousePage.js
+++ src/components/pages/HousePage.js
@@ -1,15 +1,47 @@
 import React, {useEffect, useState} from "react";
 import HouseItem from "../items/HouseItem";
-function HousePage(){
+import { GrFormAdd } from 'react-icons/gr';
+
+
+function HousePage({id}){
 
     const [houses, setHouses] = useState([])
+    const [image_url, setImage_url] = useState('')
+    const [house_number, setHouse_number] = useState('')
+    const [description, setDescription] = useState('')
+    const [rent, setRent] = useState(0.00)
 
+    
 
     useEffect(()=>{
-        fetch("http://127.0.0.1:3000/reviews")
+        fetch(`http://127.0.0.1:3000//apartments/${id}/houses`)
         .then((r) => r.json())
         .then((r)=>setHouses(r.data))
-    },[])
+    },[id])
+
+
+    // /apartment/house/add/:id
+
+
+    function handleSubmit(e){
+		e.preventDefault();
+		fetch("http://127.0.0.1:3000/tenants/register", {
+			mode: 'no-cors',
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({rent, description, house_number, image_url}),
+		})
+        .then((r) => {
+            if (r.status.created) {
+              r.json().then((house) => {
+                setHouses(...houses, house)
+
+            });
+            }
+          });
+	}
 
 
 
@@ -19,32 +51,35 @@ function HousePage(){
             <div className="container  mt-4" id="selection">
                 
                 <div className="row">
-                    <center><h2 style={{color:"red"}} id="reviewform">Leave a review </h2></center>
+                    <center><h2 style={{color:"red"}} id="reviewform">Add a house </h2></center>
                 
-                   {/* contain a form to add a reviews */}
     
                    <form className="row gx-3 gy-2 align-items-center" id="create_form" onSubmit={handleSubmit}>
                         <div className="col-sm-3">
-                    <label className="visually-hidden" for="specificSizeInputName">Title</label>
-                     <input type="text" className="form-control" id="specificSizeInputName" placeholder="Title" Value={title} onChange={(e)=>setTitle(e.target.value)}/>
-                </div>
-                <div className="col-sm-6">
-                    <label className="visually-hidden" for="specificSizeInputGroupUsername">Description</label>
-                    <div className="input-group">
+                            <label className="visually-hidden" for="specificSizeInputName">house number</label>
+                            <input type="text" className="form-control" id="specificSizeInputName" placeholder="Title" Value={house_number} onChange={(e)=>setHouse_number(e.target.value)}/>
+                        </div>
+                        <div className="col-sm-3">
+                            <label className="visually-hidden" for="specificSizeInputGroupUsername">Description</label>
+                            <div className="input-group">
                             <input type="text" className="form-control" id="specificSizeInputGroupUsername" placeholder="Description" value={description} onChange={(e)=> setDescription(e.target.value)} />
-                    </div>
-                    </div>
-                    <div className="col-sm-2">
-                        <label className="visually-hidden" for="specificSizeSelect">Priority</label>
-                        <select className="form-select" id="specificSizeSelect" value={priority} onChange={(e)=> setPriority(e.target.value)}>
-                            <option selected>Rating...</option>
-                            <option value="1">⭐</option>
-                            <option value="2">⭐⭐</option>
-                            <option value="3">⭐⭐⭐</option>
-                            <option value="4">⭐⭐⭐⭐</option>
-                            <option value="5">⭐⭐⭐⭐⭐</option>
-                        </select>
-                    </div>
+                            </div>
+                        </div>
+
+                        <div className="col-sm-3">
+                            <label className="visually-hidden" for="specificSizeInputGroupUsername">rent</label>
+                            <div className="input-group">
+                            <input type="number" className="form-control" id="specificSizeInputGroupUsername" placeholder="Description" value={rent} onChange={(e)=> setRent(e.target.value)} />
+                            </div>
+                        </div>
+
+                        <div className="col-sm-3">
+                            <label className="visually-hidden" for="specificSizeInputGroupUsername">image url</label>
+                            <div className="input-group">
+                            <input type="number" className="form-control" id="specificSizeInputGroupUsername" placeholder="Description" value={image_url} onChange={(e)=> setImage_url(e.target.value)} />
+                            </div>
+                        </div>
+
                     <div className="col-1">
                          <button type="submit" className="btn btn-primary"><GrFormAdd/></button>
                     </div>
@@ -64,7 +99,7 @@ function HousePage(){
             <div  className="row">
 
                 {
-                    houses.map((house)=> <HouseItem house={house }/> )
+                    houses.map((house)=> <HouseItem house={house}/> )
                 }
 
 
