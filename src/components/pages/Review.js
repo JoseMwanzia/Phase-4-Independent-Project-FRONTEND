1ab6ceb0e8db09c4a5d56829b2ae8ddba5f22308
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-02 00:27:46 -0700

    fix review page

--- src/components/pages/Review.js
+++ src/components/pages/Review.js
@@ -1,10 +1,14 @@
 import React, {useEffect, useState} from "react";
 import './Review.css'
 import ReviewItem from "../items/ReviewItem";
+import { GrFormAdd} from "react-icons/gr"
 
 function Review(){
 
     const [reviews, setReviews] = useState([])
+    const [rating, setRating] =useState(1)
+    const [description, setDescription] =useState("")
+
 
 
     useEffect(()=>{
@@ -12,6 +16,27 @@ function Review(){
         .then((r) => r.json())
         .then((r)=>setReviews(r.data))
     },[])
+
+
+    function handleSubmit(e){
+		e.preventDefault();
+		fetch("http://127.0.0.1:3000/tenants/review/add", {
+			mode: 'no-cors',
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({rating, description}),
+		})
+        .then((r) => {
+            if (r.status.created) {
+              r.json().then((review) => {
+                setReviews(...reviews, review)
+
+            });
+            }
+          });
+	}
 
 
 
@@ -27,25 +52,14 @@ function Review(){
     
                    <form className="row gx-3 gy-2 align-items-center" id="create_form" onSubmit={handleSubmit}>
                         <div className="col-sm-3">
-                    <label className="visually-hidden" for="specificSizeInputName">Title</label>
-                     <input type="text" className="form-control" id="specificSizeInputName" placeholder="Title" Value={title} onChange={(e)=>setTitle(e.target.value)}/>
+                    <label className="visually-hidden" for="specificSizeInputName">rating</label>
+                     <input type="number" className="form-control" id="specificSizeInputName" placeholder="Title" Value={rating} onChange={(e)=>setRating(e.target.value)}/>
                 </div>
                 <div className="col-sm-6">
                     <label className="visually-hidden" for="specificSizeInputGroupUsername">Description</label>
                     <div className="input-group">
                             <input type="text" className="form-control" id="specificSizeInputGroupUsername" placeholder="Description" value={description} onChange={(e)=> setDescription(e.target.value)} />
                     </div>
-                    </div>
-                    <div className="col-sm-2">
-                        <label className="visually-hidden" for="specificSizeSelect">Priority</label>
-                        <select className="form-select" id="specificSizeSelect" value={priority} onChange={(e)=> setPriority(e.target.value)}>
-                            <option selected>Rating...</option>
-                            <option value="1">⭐</option>
-                            <option value="2">⭐⭐</option>
-                            <option value="3">⭐⭐⭐</option>
-                            <option value="4">⭐⭐⭐⭐</option>
-                            <option value="5">⭐⭐⭐⭐⭐</option>
-                        </select>
                     </div>
                     <div className="col-1">
                          <button type="submit" className="btn btn-primary"><GrFormAdd/></button>
